a954737fe479d0055fe6f24a5ec9bcfc178421ab
mhadziq0403 <mhadziq0403@gmail.com>
2022-10-19 20:53:09 +0700

    improves code, yang sebelumnya ternyata jelek banget HAHA

--- Greedy.java
+++ Greedy.java
@@ -3,10 +3,7 @@ import java.util.*;
 
 public class Greedy {
     private static Long[][] memo;
-    private static int num;
     private static int[] coins;
-    private static long firstPick; // coin pertama yang dipilih
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,55 +11,56 @@ public class Greedy {
         // masukin koin yang available
         coins = new int[]{25, 21, 10, 1};
 
-        num = scanner.nextInt();
+        int num = scanner.nextInt();
         memo = new Long[num][2]; // 0 = banyak coin; 1 = coin pertama
-        System.out.println("Coins needed : " + g(num));
+        System.out.println("Coins needed: " + g(num));
 
         // cetak coin apa aja
-        System.out.println("What are those :");
+        System.out.println("What are those:");
         int temp = num;
+        long current = -1;
+        int countCoin = 0;
         while (temp != 0) {
-            System.out.println(memo[temp-1][1]);
+            if (current == memo[temp-1][1]) {
+                countCoin++;
+            } else {
+                if (current != -1) {
+                    System.out.println(countCoin + "x coin " + current);
+                }
+                current = memo[temp-1][1];
+                countCoin = 1;
+            }
+
             temp -= memo[temp-1][1];
         }
+        System.out.println(countCoin + "x coin " + current);
     }
 
     static long g(int ret) {
         if (ret == 0) {
             return 0;
         }
-        if (ret < 0) {
-            return num; // biar gede dan ga kepilih di bawah
-        }
 
         if (memo[ret - 1][0] != null) {
             return memo[ret - 1][0];
         }
 
-        long minCoin = Long.MAX_VALUE;
+        long minCoin = 0;
+        long minNeeds = Long.MAX_VALUE;
         long temp;
-        long[] options = new long[coins.length]; // opsi cabang coin
 
         // iterasi tiap opsi coin
-        for (int i = 0; i < coins.length; i++) {
-            temp = 1 + g(ret - coins[i]);
-            minCoin = Math.min(minCoin, temp);
-            options[i] = temp;
-        }
-
-        temp = findMin(options);
-        memo[ret - 1][1] = firstPick;
-        return memo[ret - 1][0] = temp;
-    }
-
-    public static long findMin(long[] arr) {
-        long min = Long.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-                firstPick = coins[i];
+        for (int coin : coins) {
+            if (coin <= ret) {
+                temp = 1 + g(ret - coin);
+                if (temp < minNeeds) {
+                    minNeeds = temp;
+                    minCoin = coin;
+                }
             }
         }
-        return min;
+
+        memo[ret - 1][1] = minCoin;
+        return memo[ret - 1][0] = minNeeds;
     }
 }
